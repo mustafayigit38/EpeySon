5acc250605a9dcb6a1b0f1483458fe4a6f9115b0
mustafayigit38 <csmustafa887@gmail.com>
2024-05-14 11:24:18 +0300

    epey eklendi

---
+++ Epey/WebApplication1/Controllers/TelefonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TelefonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AkıllıTelefon() 
+        {
+            return View();
+        }
+        public IActionResult TusluTelefon ()
+        {
+            return View();
+        }
+        public IActionResult AkıllıSaat()
+        {
+            return View();
+        }
+        public IActionResult SarjAleti()
+        {
+            return View();
+        }
+
+    }
+}
