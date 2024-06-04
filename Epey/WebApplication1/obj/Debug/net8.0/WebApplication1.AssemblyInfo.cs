00c903797e1d0b117792afc5665de9e065c74605
mustafayigit38 <csmustafa887@gmail.com>
2024-06-04 07:50:31 +0300

    ds

--- Epey/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Epey/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c812bd7b311145817fb424fb6ce5725fc556fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b619cc688aa49300a0e552a7f023446fab44426")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
